71cc237009093f47092732c5ed07a91276dc1b5c
modugulapushpitha <51950141+modugulapushpitha@users.noreply.github.com>
2019-08-29 21:35:17 +0530

    Update UserRepository.java

--- User-Registration/src/main/java/com/stackroute/repository/UserRepository.java
+++ User-Registration/src/main/java/com/stackroute/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.stackroute.repository;
 import com.stackroute.domain.UserDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@Repository
 public interface UserRepository extends MongoRepository<UserDetails,String> {
 }
