c617fea507572bd7348a02385f3d8012aa75a4c1
NavyaSriSura <navyasura98@gmail.com>
2019-09-03 10:31:59 +0530

    first commit without kafka

--- Data-Cleaning-Microservice/src/main/java/com/stackroute/App.java
+++ Data-Cleaning-Microservice/src/main/java/com/stackroute/App.java
@@ -2,9 +2,6 @@ package com.stackroute;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.stackroute.domain.Media1;
-import com.stackroute.domain.Tweet1;
-import com.stackroute.domain.TwitterURL1;
-import com.stackroute.domain.User1;
 
 /**
  * Hello world!
@@ -13,13 +10,9 @@ import com.stackroute.domain.User1;
 public class App 
 {
     public static void main( String[] args ) throws JsonProcessingException {
-        User1 user1=new User1();
-        user1.userFilter();
-        Tweet1 tweet1=new Tweet1();
-        tweet1.tweetFilter();
+
         Media1 media1=new Media1();
         media1.mediaFilter();
-        TwitterURL1 twitterURL1= new TwitterURL1();
-        twitterURL1.twitterURLFilter();
+
     }
 }
