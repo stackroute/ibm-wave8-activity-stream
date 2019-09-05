5ec214831961820eb857b6fe48611135287137dc
prathyusha36 <lprathyusha97@gmail.com>
2019-09-05 15:05:06 +0530

    Upstream and Downstream Service triggered from a Single App

--- activity-stream-app/mainapp/src/main/java/com/stackroute/App.java
+++ activity-stream-app/mainapp/src/main/java/com/stackroute/App.java
@@ -1,13 +1,23 @@
 package com.stackroute;
 
+import com.stackroute.upstream.ASUpstreamApp;
+import com.techprimers.kafka.springbootkafkaconsumerexample.SpringBootKafkaConsumerExampleApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@Import({ASUpstreamApp.class, SpringBootKafkaConsumerExampleApplication.class})
 public class App 
 {
     public static void main( String[] args )
     {
+        SpringApplication.run(App.class,args);
+        //SpringApplication.run();
         System.out.println( "Hello World!" );
     }
 }
