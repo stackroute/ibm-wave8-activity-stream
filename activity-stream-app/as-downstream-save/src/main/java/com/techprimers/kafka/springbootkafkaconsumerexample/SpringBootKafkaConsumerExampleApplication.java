e48ce87f13fd6afe47d96fbbfdb3244dae80e8c0
prathyusha36 <lprathyusha97@gmail.com>
2019-09-05 16:19:11 +0530

    modified pom.xml to use user-defined dependency from .m2 repository

--- activity-stream-app/as-downstream-save/src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/SpringBootKafkaConsumerExampleApplication.java
+++ activity-stream-app/as-downstream-save/src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/SpringBootKafkaConsumerExampleApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@Import({ASUpstreamApp.class})
+//@Import({ASUpstreamApp.class})
 public class SpringBootKafkaConsumerExampleApplication {
 
 	public static void main(String[] args) {
